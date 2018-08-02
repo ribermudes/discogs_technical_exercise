ae36d6a469c9187dc51ffc82f92d99e63a4c3a2a
Rachael Bermudes <bermudesorma@gmail.com>
2018-08-01 18:07:43 -0700

    add to js file

--- scripts.js
+++ scripts.js
@@ -1,16 +1,39 @@
-const data = async () => new Promise(resolve => {
-  setTimeout(() => resolve(window.apiResponse), 1000)
-})
+var request = new XMLHttpRequest();
 
-const sortReleases = e => {
-  const { value='name' } = e.target
-  console.log('sorted', value);
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://api.discogs.com/releases/2863', true);
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.stringify(window.apiResponse);
+
+  data.forEach(releases => {
+  // Log each movie's title
+  console.log(releases.year);
+});
+
 }
 
-domready(async () => {
-  const $sort = document.getElementById('sort')
-  $sort.addEventListener('change', sortReleases)
-  
-  const response = await data()
-  console.log(`${response.releases.length} Releases`)
-})
+
+// Send request
+request.send();
+
+
+
+
+// const data = async () => new Promise(resolve => {
+//   setTimeout(() => resolve(window.apiResponse), 1000)
+// })
+//
+// const sortReleases = e => {
+//   const { value='name' } = e.target
+//   console.log('sorted', value);
+// }
+//
+// domready(async () => {
+//   const $sort = document.getElementById('sort')
+//   $sort.addEventListener('change', sortReleases)
+//
+//   const response = await data()
+//   console.log(`${response.releases.length} Releases`)
+// })
